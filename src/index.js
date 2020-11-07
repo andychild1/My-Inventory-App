98dfb9d29bae089df8397a75d5eae386aba0ea22
Andrea Tosetto <toxand@MacPro-800.local>
2020-11-07 12:01:03 +0100

    prod

--- src/index.js
+++ src/index.js
@@ -7,7 +7,9 @@ import { Auth0Provider } from '@auth0/auth0-react';
 const domain = process.env.REACT_APP_AUTH0_DOMAIN;
 const clientId = process.env.REACT_APP_AUTH0_CLIENT_ID;
 
+
 ReactDOM.render(
+  process.env.NODE_ENV === 'production' && (
   <Auth0Provider 
   domain={domain}
   clientId={clientId}
@@ -17,5 +19,5 @@ ReactDOM.render(
     </Auth0Provider>,
   
   document.getElementById('root')
-  
+  )
 );
