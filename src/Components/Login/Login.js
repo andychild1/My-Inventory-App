1dabbd7884958929985f8f262ba45df0aefcf981
andychild1 <toxand@gmail.com>
2021-08-23 11:41:10 +0200

    changed instructions for users

--- src/Components/Login/Login.js
+++ src/Components/Login/Login.js
@@ -11,7 +11,7 @@ const Login = () => {
             !isAuthenticated && (
                 <div className="login">
                  <h1>Welcome to my Inventory App</h1>
-             <h1>Click Start to sign up</h1>
+             <h1>Start to sign up</h1>
             <button className="start" onClick={() => loginWithRedirect()}>
                 START
             </button>
