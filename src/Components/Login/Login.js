d20d8709de623f93cda9b9a5b6ef70b0b252adbd
Andrea <toxand@gmail.com>
2021-08-23 13:45:40 +0200

    Updated second header

--- src/Components/Login/Login.js
+++ src/Components/Login/Login.js
@@ -11,7 +11,7 @@ const Login = () => {
             !isAuthenticated && (
                 <div className="login">
                  <h1>Welcome to my Inventory App</h1>
-             <h2>Start to sign up</h2>
+             <h2>Start to begin</h2>
             <button className="start" onClick={() => loginWithRedirect()}>
                 START
             </button>
