b81d581363fe07137c8e06e415877ac8a05eb793
andychild1 <toxand@gmail.com>
2021-08-23 15:26:58 +0200

    improved colors and animations

--- src/Components/Login/Login.js
+++ src/Components/Login/Login.js
@@ -11,7 +11,7 @@ const Login = () => {
             !isAuthenticated && (
                 <div className="login">
                  <h1>Welcome to my Inventory App</h1>
-             <h1>Start to begin</h1>
+             <h2>Press Start to begin</h2>
             <button className="start" onClick={() => loginWithRedirect()}>
                 START
             </button>
